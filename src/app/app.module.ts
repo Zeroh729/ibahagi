1f205efc14b77bbacb800b8de2f2f50c4936022a
Zeroh729 <joyceplayingcp@yahoo.com>
2017-05-04 18:50:39 +0800

    added menu and map template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TweetClassifierComponent } from './tweetclass-app.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,
+  					TweetClassifierComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/tweetclass-app.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector : "tweet-classifier",
+	templateUrl : "./tweetclass-app.component.html",
+	styleUrls : [ "./tweetclass-app.component.css" ]
+})
+
+export class TweetClassifierComponent{
+
+}
